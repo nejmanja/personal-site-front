ab9b1ada1b339869ca2e980a4ec8b4289b9caa85
Nemanja Milanović <neca1mesto@gmail.com>
2022-10-09 23:37:13 +0200

    Create new scrollable project gallery; fix vscode complaining about babel

--- components/CarouselItem.jsx
+++ components/CarouselItem.jsx
@@ -1,42 +1,52 @@
 import React from "react";
 import { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 import styles from "./CarouselItem.module.css";
 import utilStyles from "../styles/utils.module.css";
 
-export default function CarouselItem({ className, title, thumbnail, accent }) {
-    const [hover, setHover] = useState(false);
+export default function CarouselItem({
+	className,
+	title,
+	thumbnail,
+	accent,
+	link,
+}) {
+	const [hover, setHover] = useState(false);
 
-    return (
-        <div
-            className={`${styles.carouselItem} ${className}`}
-            onMouseEnter={() => {
-                setHover(true);
-            }}
-            onMouseLeave={() => {
-                setHover(false);
-            }}
-            href="#"
-        >
-                <div className={styles.carouselItemThumbContainer}>
-                    <img
-                        className={styles.carouselItemThumb}
-                        src={thumbnail}
-                        alt="project thumbnail"
-                    />
-                </div>
-                <h1
-                    className={`
-                        ${styles.carouselItemTitle} ${utilStyles.textWhite} ${
-                        utilStyles.ffSansCond
-                    } ${utilStyles.bold} ${utilStyles.fs500} ${
-                        styles[`carouselItemTitle_${accent}`]
-                    }
-                        ${hover && styles.carouselItemTitle_active}`}
-                >
-                    {title}
-                </h1>
-        </div>
-    );
+	return (
+		<Link href={link}>
+			<div
+				className={`${styles.carouselItem} ${className}`}
+				onMouseEnter={() => {
+					setHover(true);
+				}}
+				onMouseLeave={() => {
+					setHover(false);
+				}}
+			>
+				<div
+					className={`${styles.thumbContainer} ${styles[`thumbContainer_${accent}`]} 
+                        ${hover && styles.active}`}
+				>
+					<Image
+						src={thumbnail}
+						layout="fill"
+						objectFit="cover"
+						alt="project thumbnail"
+					/>
+				</div>
+				<p
+					className={`
+                        ${styles.title} ${utilStyles.textWhite} 
+                        ${utilStyles.ffSansCond} 
+                        ${utilStyles.bold} ${utilStyles.fs400} 
+                        ${styles[`title_${accent}`]}`}
+				>
+					{title}
+				</p>
+			</div>
+		</Link>
+	);
 }
--- components/Showcase.jsx
+++ components/Showcase.jsx
@@ -3,20 +3,81 @@ import CarouselItem from "./CarouselItem";
 
 import styles from "./Showcase.module.css";
 import utilStyles from "../styles/utils.module.css";
+import ScrollableList from "./ScrollableList";
 
 export default function Showcase() {
 	return (
-		<div className={`${styles.showcase} ${utilStyles.container} ${utilStyles.fullscreenPanel}`}>
-			<h1 className={`${utilStyles.uppercase} ${utilStyles.bold} ${utilStyles.fs800} ${utilStyles.ffSansCond} ${utilStyles.textLight}`}>
+		<div
+			className={`${styles.showcase} ${utilStyles.container} ${utilStyles.fullscreenPanel}`}
+		>
+			<h1
+				className={`${utilStyles.uppercase} ${utilStyles.bold} ${utilStyles.fs800} ${utilStyles.ffSansCond} ${utilStyles.textLight}`}
+			>
 				Still not convinced?
 			</h1>
 			<h2>Check out some of my past projects!</h2>
-			<div className={utilStyles.flex}>
-				<CarouselItem title="Sample project" thumbnail="/images/thumb.png" accent="r"/>
-				<CarouselItem title="A really long sample project" thumbnail="/images/thumb2.jpg" accent="g"/>
-				<CarouselItem title="Sample project" thumbnail="/images/thumb3.png" accent="b"/>
-				<CarouselItem title="Sample project" thumbnail="/images/thumb.png" />
-			</div>
+			<ScrollableList
+				listItems={[
+					<CarouselItem
+						title="Sample project"
+						thumbnail="/images/thumb.png"
+						accent="r"
+						link="#"
+					/>,
+					<CarouselItem
+						title="Slightly bigger title"
+						thumbnail="/images/thumb2.jpg"
+						accent="g"
+						link="#"
+					/>,
+					<CarouselItem
+						title="A really elaborate and massive title, you cannnot even comprehend how big it is"
+						thumbnail="/images/thumb3.png"
+						accent="b"
+						link="#"
+					/>,
+					<CarouselItem
+						title="Sample project"
+						thumbnail="/images/thumb3.png"
+						link="#"
+					/>,
+					<CarouselItem
+						title="Sample project"
+						thumbnail="/images/thumb3.png"
+						accent="b"
+						link="#"
+					/>,
+					<CarouselItem
+						title="Sample project"
+						thumbnail="/images/thumb.png"
+						accent="r"
+						link="#"
+					/>,
+					<CarouselItem
+						title="Slightly bigger title"
+						thumbnail="/images/thumb2.jpg"
+						accent="g"
+						link="#"
+					/>,
+					<CarouselItem
+						title="A really elaborate and massive title, you cannnot even comprehend how big it is"
+						thumbnail="/images/thumb3.png"
+						accent="b"
+						link="#"
+					/>,
+					<CarouselItem
+						title="Sample project"
+						thumbnail="/images/thumb3.png"
+						link="#"
+					/>,
+					<CarouselItem
+						title="Sample project"
+						thumbnail="/images/thumb3.png"
+						accent="b"
+						link="#"
+					/>,
+				]}
+			/>
 		</div>
 	);
 }
